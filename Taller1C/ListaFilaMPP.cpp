8642b4ccb89cb1cb3b950daff8daf4caefbe55b9
BaironR <blrojas2668@gmail.com>
2023-09-10 16:20:02 -0300

    commit2

--- Taller1C/ListaFilaMPP.cpp
+++ Taller1C/ListaFilaMPP.cpp
@@ -2,6 +2,7 @@
 
 ListaFilaMPP::ListaFilaMPP(){ 
 	this->cabeza = new NodoMPP(NULL, NULL, 0);
+	this->cabeza->set_izquierdo(this->cabeza);
 }
 
 void ListaFilaMPP::agregar(Vehiculo* vehiculo, int fila, int columna){
@@ -10,16 +11,24 @@ void ListaFilaMPP::agregar(Vehiculo* vehiculo, int fila, int columna){
 
 		NodoMPP* nodo = new NodoMPP(vehiculo, fila, columna);
 
-		if (this->cabeza->get_izquierdo() == NULL) {
-			this->cabeza->set_izquierdo(nodo);
+		if (this->cabeza->get_izquierdo() == this->cabeza){
 			nodo->set_izquierdo(this->cabeza);
+			this->cabeza->set_izquierdo(nodo);
 			return;
 		}
 
-		NodoMPP* aux = this->cabeza;
+		NodoMPP* aux = this->cabeza->get_izquierdo();
 
-		while (aux->get_izquierdo() != NULL) {
+		while (aux != this->cabeza){
 
+			if (columna == aux->get_columna() && fila == aux->get_fila()){
+				return;
+			}
+
+			else if (columna > aux->get_columna() && ) {
+
+			}
+			aux = aux->get_izquierdo();
 		}
 
 	}
